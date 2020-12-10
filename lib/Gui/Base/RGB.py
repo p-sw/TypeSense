145bf6dfc60b2abf815af287f323b9bcf9c0c02e
sserve-kr <csgo.sserveg@gmail.com>
2020-12-10 13:27:52 +0900

    Added add_color() function in RGB class

--- lib/Gui/Base/RGB.py
+++ lib/Gui/Base/RGB.py
@@ -6,3 +6,8 @@ class RGB:
 
     def get(self, t: type = list):
         return t([self.red, self.green, self.blue])
+
+    def add_color(self, r: int, g: int, b: int):
+        self.red += int(r)
+        self.green += int(g)
+        self.blue += int(b)
