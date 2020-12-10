3073ba67846ce3651a0702eae263659356a58b6c
sserve-kr <csgo.sserveg@gmail.com>
2020-12-10 13:39:36 +0900

    Removed add_color() in RGB class

--- lib/Gui/Base/RGB.py
+++ lib/Gui/Base/RGB.py
@@ -6,6 +6,3 @@ class RGB:
 
     def get(self, t: type = list):
         return t([self.red, self.green, self.blue])
-
-    def add_color(self, r: int, g: int, b: int):
-        return RGB(self.red + int(r), self.green + int(g), self.blue + int(b))
